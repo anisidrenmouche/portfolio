5e08d2c64c8d83a4bf4a5277823c8d85eac3612f
anis idrenmouche <anisidrenmouche@hotmail.fr>
2019-04-15 11:39:46 +0200

    correction de code

--- mail.php
+++ mail.php
@@ -1,17 +1,17 @@
 
 <? php
 si ( $ _POST ) {
-  $fisrtname = $ _POST ['fisrtname'];
-  $ email  =  $ _POST [ ' email ' ];
-  $ name  =  $ _POST [ ' name ' ];
-  $ message  =  $ _POST [ ' message ' ];
-  $ headers  =  " Version MIME: 1.0 \ r \ n " ; // header mail format 
-  $ headers  . =  " Type de contenu: text / plain; charset = iso-8859-1 \ r \ n " ;
-  $ headers  . =  " De: $ nom < $ email > \ r \ n Répondre à: $ nom < $ email > \ n X-Mailer: PHP " ;
-  $ subject = " $ objet " ;   
-  $ destinataire = " anisidrenmouche@hotmail.fr " ;
-  $ body = " $ message " ;
-  if ( mail ( $ destinataire , $ sujet , $ corps , $ en-têtes )) {
+  $fisrtname = $_POST ['fisrtname'];
+  $email  =  $_POST [ 'email' ];
+  $name  =  $_POST ['name' ];
+  $message  =  $_POST ['message' ];
+  $ headers  =  "Version MIME: 1.0\r\n" ; // header mail format
+  $ headers  . =  "Type de contenu: text / plain; charset = iso-8859-1\r\n" ;
+  $ headers  . =  "De: $nom <$email>\r\n Répondre à: $nom <$email>\n X-Mailer: PHP " ;
+  $subject = "Prise de contact depuis le porte folio" ;
+  $destinataire = "anisidrenmouche@hotmail.fr" ;
+  $body = $message;
+  if ( email ( $destinataire , $subject , $body , $en-têtes )) {
     $ response [ ' status ' ] =  ' success ' ;
     $ response [ ' msg ' ] =  ' votre courrier est envoyé ' ;
   } else {
