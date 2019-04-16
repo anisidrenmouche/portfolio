3c929c750054246d52f58599cd9bce32796ba7fc
Anis <48013666+anisidrenmouche@users.noreply.github.com>
2019-04-16 17:40:19 +0200

    Nettoyage de code

--- mail.php
+++ mail.php
@@ -1,22 +1,22 @@
 
 <? php
-if ( $_POST ) {
-  $fisrtname = $_POST ['fisrtname'];
-  $email  =  $_POST ['email'];
-  $name  =  $_POST ['name'];
-  $message  =  $_POST ['message'];
-  $headers  =  "Version MIME: 1.0\r\n" ; // header mail format
-  $headers  .=  "Type de contenu: text / plain; charset = iso-8859-1\r\n" ;
-  $headers  .=  "De: $nom <$email>\r\n Répondre à: $nom <$email>\n X-Mailer: PHP" ;
-  $subject = "Prise de contact depuis le porte folio" ;
-  $destinataire = "anisidrenmouche@hotmail.fr" ;
-  if ( email ( $destinataire , $subject , $message , $headers )) {
-    $response ['status'] =  'success' ;
-    $response ['msg'] =  'votre courrier est envoyé' ;
+if ($_POST) {
+  $fisrtname = $_POST['fisrtname'];
+  $email  =  $_POST['email'];
+  $name  =  $_POST['name'];
+  $message  =  $_POST['message'];
+  $headers  =  "Version MIME: 1.0\r\n"; // header mail format
+  $headers  .=  "Type de contenu: text / plain; charset = iso-8859-1\r\n";
+  $headers  .=  "De: $nom <$email>\r\n Répondre à: $nom <$email>\n X-Mailer: PHP";
+  $subject = "Prise de contact depuis le porte folio";
+  $destinataire = "anisidrenmouche@hotmail.fr";
+  if (email($destinataire, $subject, $message, $headers)) {
+    $response['status'] = 'success' ;
+    $response['msg'] = 'votre courrier est envoyé' ;
   } else {
-    $response ['status'] =  'error' ;
-    $response ['msg'] =  'Une erreur' ;
+    $response['status'] = 'error' ;
+    $response['msg'] =  'Une erreur' ;
   }
-  echo  json_encode($response);
+  echo json_encode($response);
 }
 ? >
