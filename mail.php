3985097d4edaf325586fc7abc23ebc6c275dcc42
Anis <48013666+anisidrenmouche@users.noreply.github.com>
2019-04-16 17:45:47 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,10 +4,13 @@ if ($_POST) {
   $fisrtname = $_POST['fisrtname'];
   $email  =  $_POST['email'];
   $name  =  $_POST['name'];
-  $message  =  $_POST['message'];
+  
   $headers  =  "Version MIME: 1.0\r\n"; // header mail format
   $headers  .=  "Type de contenu: text / plain; charset = iso-8859-1\r\n";
   $headers  .=  "De: $nom <$email>\r\n Répondre à: $nom <$email>\n X-Mailer: PHP";
+  
+  $message  =  $_POST['message'] . "\r\n Mail de : " . $firstname . " " . $name;
+  
   $subject = "Prise de contact depuis le porte folio";
   $destinataire = "anisidrenmouche@hotmail.fr";
   if (email($destinataire, $subject, $message, $headers)) {
